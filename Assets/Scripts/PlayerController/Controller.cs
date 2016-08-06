dfdba5ab64f72af27aad40ec7c2c9fb38d8211b8
karlovvemail <karlovvemail@yandex.ua>
2016-08-06 13:44:09 +0300

    Смена оружия

--- Assets/Scripts/PlayerController/Controller.cs
+++ Assets/Scripts/PlayerController/Controller.cs
@@ -1,7 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Specialized;
 
 public class Controller : MonoBehaviour {
+
+
+	public GameObject gun1;
+	public GameObject gun2;
+
+	public int curWeapon;
 
 	// Use this for initialization
 	void Start () {
@@ -10,6 +17,47 @@ public class Controller : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		SelectGun ();
 	
 	}
+	void SelectGun(){
+		if (Input.GetAxis ("Mouse ScrollWheel") < 0 || Input.GetKeyDown("1"))
+		{
+			curWeapon -= 1;
+			Switch ();
+		}
+		if (Input.GetAxis ("Mouse ScrollWheel") > 0 || Input.GetKeyDown("2"))
+		{
+			curWeapon += 1;
+			Switch ();
+		}
+		if (curWeapon > 2) {
+			curWeapon = 1;
+			Switch ();
+		}
+		if (curWeapon <1) {
+			curWeapon = 2;
+			Switch ();
+		}
+
+	
+
+	}
+	void Null(){
+		gun1.SetActive (false);
+		gun2.SetActive (false);
+			}
+	void Switch(){
+		if (curWeapon == 1) 
+		{
+			Null ();
+			gun1.SetActive (true); 
+		}
+		if (curWeapon == 2) 
+		{
+			Null ();
+			gun2.SetActive (true); 
+		}
+	}
+
 }
