9612d3b9411156c38b1082171c644155a9fecfe0
ValeryPonomarenko <con-bo@yandex.ru>
2016-08-08 19:57:20 +0300

    Добавил абстрактный класс оружия. Сделал 2 класса, которые реализуют пистолет и автомат.

---
+++ Assets/Scripts/Weapons/AbstractWeapon.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public abstract class AbstractWeapon : MonoBehaviour
+{
+    public int MaxBullets;
+    public int MaxBulletsInHolder;
+    public int Damage;
+    protected int Bullets;
+    protected int BulletsInHolder;
+
+    public abstract void Shoot();
+
+    public void Reload()
+    {
+        if (MaxBulletsInHolder - BulletsInHolder >= Bullets)
+        {
+            BulletsInHolder += Bullets;
+            Bullets = 0;
+        }
+        else
+        {
+            Bullets -= MaxBulletsInHolder - BulletsInHolder;
+            BulletsInHolder = MaxBulletsInHolder;
+        }
+    }
+}
---
+++ Assets/Scripts/Weapons/RifleWeapon.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class RifleWeapon : AbstractWeapon
+{
+    public override void Shoot()
+    {
+        Debug.Log("Rifle pew-pew-pew");
+    }
+}
